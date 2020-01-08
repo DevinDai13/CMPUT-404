b5ad4b0e319c6806f7cbc4924dc30b30f45732ec
Wen Dai <wdai@cs.ualberta.ca>
2020-01-07 18:16:19 -0700

    updated script

--- 404lab1.py
+++ 404lab1.py
@@ -4,3 +4,7 @@ print(requests.__version__)
 
 requests.get("https://www.google.com")
 
+var = requests.get("https://raw.githubusercontent.com/DevinDai13/CMPUT-404/Lab1/404lab1.py")
+
+print(var.content)
+
